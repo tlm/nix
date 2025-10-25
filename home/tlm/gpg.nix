8ff6fc16bc95648be6a76caf088a7118268461ac
tlm <thomas@tlm.id.au>
2025-10-25 14:56:28 +0000

    disable gpg-agent
    I have to make sure the gpg-agent is disabled at the moment to support
    remote ssh dev. Will figure out a way to have local and remote
    forwarding down the line.

--- home/tlm/gpg.nix
+++ home/tlm/gpg.nix
@@ -1,7 +1,12 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+{
   programs.gpg = {
     enable = true;
     settings = {
     };
+  };
+
+  programs.gpg-agent = {
+    enable = false;
   };
 }
