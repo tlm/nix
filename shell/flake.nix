2d3e4d7fac76e6490b998b74bf3a136201e0813b
tlm <thomas@tlm.id.au>
2025-10-26 10:00:12 +0000

    tidy up os shell flake.nix had a few problems with it.

--- shell/flake.nix
+++ shell/flake.nix
@@ -15,15 +15,20 @@
       nixpkgs-juju-mongo,
     }:
     let
-      supportedSystems = ["x86_64-linux" "aarch64-linux"];
-    in {
-      devShells = nixpkgs.lib.genAttrs supportedSystems (system:
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs supportedSystems (
+        system:
         let
           pkgs = import nixpkgs {
             inherit system;
             config.allowUnfree = true;
           };
-          pkgs-unstable = import nixpkgs {
+          pkgs-unstable = import nixpkgs-unstable {
             inherit system;
             config.allowUnfree = true;
           };
@@ -33,10 +38,12 @@
               allowUnfree = true;
             };
           };
-        in {
+        in
+        {
           esphome = import ./dev/esphome.nix {
-            inherit pkgs;
+            pkgs = nixpkgs.lib.makeExtensible pkgs;
             inherit pkgs-unstable;
+            #pkgs-unstable = nixpkgs-unstable.lib.makeExtensible pkgs-unstable;
           };
           go = import ./dev/go.nix { inherit pkgs; };
           hugo = import ./dev/hugo.nix { inherit pkgs; };
@@ -50,99 +57,4 @@
         }
       );
     };
-
-   # nixpkgs.lib.genAttrs supportedSystems (system:
-   #   let
-   #     pkgs = import nixpkgs {
-   #       inherit system;
-   #       config.allowUnfree = true;
-   #     };
-   #     pkgs-unstable = import nixpkgs {
-   #       inherit system;
-   #       config.allowUnfree = true;
-   #     };
-   #     pkgs-golang-ci = nixpkgs-golang-ci.legacyPackages.${system};
-   #     pkgs-juju-mongo = import nixpkgs-juju-mongo {
-   #       inherit system;
-   #       config = {
-   #         allowUnfree = true;
-   #       };
-   #     };
-   #   in
-   #   {
-   #     #devShells = {
-   #     #  "test" = {};
-   #     #  "x86_64-linux" = {
-   #     #    go = import ./go { inherit pkgs; };
-   #     #  };
-   #     #};
-   #     #devShells = {
-   #     #    ${system}.esphome = import ./dev/esphome.nix { inherit pkgs; inherit pkgs-unstable; };
-   #     #    ${system}.ops = import ./dev/ops.nix { inherit pkgs; };
-   #     #    ${system}.go = import ./go { inherit pkgs; };
-   #     #    ${system}.rust = import ./rust { inherit pkgs; };
-   #     #    ${system}.juju = pkgs.mkShellNoCC {
-   #     #      name = "juju-dev";
-
-   #     #      packages = [
-   #     #        pkgs.pkgsStatic.sqlite
-   #     #        pkgs.pkgsStatic.musl
-   #     #        pkgs.pkgsStatic.gcc
-   #     #        pkgs.pkgsStatic.binutils
-   #     #        #pkgs.sqlite
-   #     #        pkgs.azure-cli.out
-   #     #        pkgs.awscli2.out
-   #     #        #pkgs.autoconf
-   #     #        #pkgs.autogen
-   #     #        #pkgs.automake
-   #     #        pkgs.bash.out
-   #     #        pkgs.expect.out
-   #     #        #pkgs.gettext
-   #     #        pkgs.gh.out
-   #     #        pkgs.gnumake.out
-   #     #        pkgs.go
-   #     #        #pkgs.libtool
-   #     #        #pkgs.zig
-   #     #        #pkgs.gcc
-   #     #        #pkgs.pkgsStatic.go
-   #     #        pkgs-unstable.golangci-lint.out
-   #     #        pkgs.jq
-   #     #        #pkgs.lxc.out
-
-   #     #        #pkgs.lxd.out
-   #     #        #pkgs.pkgsMusl.musl
-   #     #        #pkgs.pkgsMusl.go
-   #     #        #pkgs.pkgsMusl.gcc
-   #     #        #pkgs.pkgsMusl.gcc
-   #     #        #pkgs.pkgsMusl.sqlite
-   #     #        #pkgs.pkgsMusl.pkg-config
-   #     #        #pkgs.minikube
-   #     #        #pkgs-juju-mongo.mongodb-4_4.out
-   #     #        pkgs.kubectl.out
-   #     #        pkgs.rootlesskit
-   #     #        pkgs.shellcheck.out
-   #     #        pkgs.shfmt.out
-   #     #        pkgs.yq-go.out
-   #     #        pkgs.zsh.out
-   #     #      ];
-
-   #     #      shellHook = ''
-   #     #        export GOBIN=''$(mktemp -d -p "" juju-go-path.XXXX)
-   #     #        echo "Using temporary GOBIN: ''${GOBIN}"
-   #     #        export PATH="''${PATH}:''${GOBIN}"
-   #     #        export GOFLAGS
-   #     #        export GOFLAGS='-ldflags=-linkmode=external -ldflags=-extldflags=-static'
-
-   #     #        if [ -z "$IN_ZSH" ]; then
-   #     #        export IN_ZSH=1
-   #     #        export SHELL=${pkgs.zsh}/bin/zsh
-   #     #        exec ${pkgs.zsh}/bin/zsh -i
-   #     #        fi
-
-   #     #        echo "Welcome to the Juju development shell!"
-   #     #      '';
-   #     #    };
-   #     #  };
-   #   }
-   # );
 }
